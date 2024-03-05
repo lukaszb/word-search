99fdfb4bedf3705fb23ded5f69648ea323600121
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2024-03-05 21:22:18 +0100

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/word-search",
   plugins: [react()],
   build: {
     sourcemap: true,
